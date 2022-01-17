57b95ca12042adc140ed4552b1fc9a71f7d3827d
Ubuntu <ubuntu@ip-172-31-87-124.ec2.internal>
2022-01-17 19:52:32 +0000

    server side

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,10 +3,11 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-import os
+import os,sys
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.append('home/ubuntu/django/myproject/')
+sys.path.append('home/ubuntu/django/myprojectenv/lib/python3.8/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
